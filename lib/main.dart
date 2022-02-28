fbfbaccd663e5cc1531c90bbad4cd6e34a246a48
tuansonhq <86151925+tuansonhq@users.noreply.github.com>
2022-02-28 16:17:28 +0700

    Update

---
+++ lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:fitness_ui_kit/pages/login_page.dart';
+import 'package:flutter/material.dart';
+import 'router.dart' as router;
+void main() {
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: LoginPage(),
+    onGenerateRoute: router.generateRoute,
+  ));
+}
